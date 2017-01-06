6a9d60bab2241a87f91e69696afe67bc5218e94c
razerjon24 <razerjon24@gmail.com>
2017-01-06 03:20:28 -0500

    fixed excel bug

--- application/views/evaluation_preview.php
+++ application/views/evaluation_preview.php
@@ -1,10 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-header("Content-type: application/vnd.ms-excel; name='excel'");
-header("Content-Disposition: attachment; filename=ficheroExcel.xls");
-header("Pragma: no-cache");
-header("Expires: 0");
-echo $_POST['datos_a_enviar'];
 ?>
 
 <head>
